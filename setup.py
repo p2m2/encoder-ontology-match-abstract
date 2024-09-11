688ecce62e1f3d20c436e73a6e2ac4513b40e604
Olivier Filangi <olivier.filangi@inrae.fr>
2024-09-11 04:21:35 +0200

    fix installation

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="LLMSemanticAnnotator",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'llm-semantic-annotator=llm_semantic_annotator.__main__:main',
