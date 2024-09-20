ab8157f7ca73149e74607666a5bc67910bcf39d9
Olivier Filangi <olivier.filangi@inrae.fr>
2024-09-20 15:55:18 +0200

    add mesh example

--- exec.sh
+++ exec.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-conffile=config/igepp-sub.json
-#rm -rf igepp-sub_w*/
-python -m llm_semantic_annotator $conffile populate_owl_tag_embeddings
-python -m llm_semantic_annotator $conffile populate_gbif_taxon_tag_embeddings
-python -m llm_semantic_annotator $conffile populate_abstract_embeddings
-python -m llm_semantic_annotator $conffile compute_tag_chunk_similarities
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <config_file>"
+    exit 1
+fi
+
+conffile="$1"
+
+run_command() {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "Erreur: La commande '$*' a échoué avec le code de sortie $status"
+        exit $status
+    fi
+}
+
+run_command python -m llm_semantic_annotator "$conffile" populate_owl_tag_embeddings
+run_command python -m llm_semantic_annotator "$conffile" populate_ncbi_taxon_tag_embeddings
+run_command python -m llm_semantic_annotator "$conffile" populate_abstract_embeddings
+run_command python -m llm_semantic_annotator "$conffile" compute_tag_chunk_similarities
 
