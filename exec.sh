7e61b7a6fa80104c2569a9f423a8b9c5ca8920b6
Olivier Filangi <olivier.filangi@inrae.fr>
2024-10-08 15:52:39 +0200

    up exec.sh

--- exec.sh
+++ exec.sh
@@ -6,6 +6,19 @@ if [ "$#" -ne 1 ]; then
 fi
 
 conffile="$1"
+venv_name="llm_semantic_annotator_env"
+
+# Fonction pour créer l'environnement virtuel s'il n'existe pas
+create_venv_if_not_exists() {
+    if [ ! -d "$venv_name" ]; then
+        echo "Creating virtual environment..."
+        python3 -m venv "$venv_name"
+        source "$venv_name/bin/activate"
+        pip install -r requirements.txt  # Assurez-vous d'avoir un fichier requirements.txt
+    else
+        source "$venv_name/bin/activate"
+    fi
+}
 
 run_command() {
     "$@"
@@ -18,13 +31,16 @@ run_command() {
 
 execute_command() {
     case $1 in
-        1) run_command python -m llm_semantic_annotator "$conffile" populate_owl_tag_embeddings ;;
-        2) run_command python -m llm_semantic_annotator "$conffile" populate_ncbi_taxon_tag_embeddings ;;
-        3) run_command python -m llm_semantic_annotator "$conffile" populate_abstract_embeddings ;;
-        4) run_command python -m llm_semantic_annotator "$conffile" compute_tag_chunk_similarities ;;
+        1) run_command python3 -m llm_semantic_annotator "$conffile" populate_owl_tag_embeddings ;;
+        2) run_command python3 -m llm_semantic_annotator "$conffile" populate_ncbi_taxon_tag_embeddings ;;
+        3) run_command python3 -m llm_semantic_annotator "$conffile" populate_abstract_embeddings ;;
+        4) run_command python3 -m llm_semantic_annotator "$conffile" compute_tag_chunk_similarities ;;
         *) echo "Invalid option" ;;
     esac
 }
+
+# Créer l'environnement virtuel s'il n'existe pas
+create_venv_if_not_exists
 
 echo "What would you like to execute?"
 echo "1. Full workflow"
@@ -36,10 +52,10 @@ read -p "Enter your choice (1-5): " choice
 
 case $choice in
     1)
-        run_command python -m llm_semantic_annotator "$conffile" populate_owl_tag_embeddings
-        run_command python -m llm_semantic_annotator "$conffile" populate_ncbi_taxon_tag_embeddings
-        run_command python -m llm_semantic_annotator "$conffile" populate_abstract_embeddings
-        run_command python -m llm_semantic_annotator "$conffile" compute_tag_chunk_similarities
+        run_command python3 -m llm_semantic_annotator "$conffile" populate_owl_tag_embeddings
+        run_command python3 -m llm_semantic_annotator "$conffile" populate_ncbi_taxon_tag_embeddings
+        run_command python3 -m llm_semantic_annotator "$conffile" populate_abstract_embeddings
+        run_command python3 -m llm_semantic_annotator "$conffile" compute_tag_chunk_similarities
         ;;
     2|3|4|5)
         execute_command $((choice - 1))
@@ -49,3 +65,6 @@ case $choice in
         exit 1
         ;;
 esac
+
+# Désactiver l'environnement virtuel à la fin
+deactivate
