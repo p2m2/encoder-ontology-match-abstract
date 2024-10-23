aff58d99564d9cbe47e7a058980e3aec442c1344
Olivier Filangi <olivier.filagi@inrae.fr>
2024-10-23 15:03:00 +0200

    fix script

--- exec-sbatch-gpu.sh
+++ exec-sbatch-gpu.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=owl_abstract_encoding_similarities
+#SBATCH --job-name=similarities_compute
 #SBATCH --chdir=/scratch/ofilangi/workspace/encoder-ontology-match-abstract
-#SBATCH --output=out_process_igepp.txt
+#SBATCH --output=planteome_annot_pubmeb.txt
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
-#SBATCH --mem=20G
+#SBATCH --mem=10G
 
 ## sbatch -p gpu exec.sh
 
-source ./check_slurm_memory.sh
+. /local/env/envpython-3.9.5.sh
+conffile=./pubmed-all.json
+./exec.sh $conffile 1
 
-/local/env/envpython-3.9.5.sh
-. env/bin/activate
-
-conffile=config/igepp.json
-
-python -m llm_semantic_annotator $conffile 1
-
-
+if [ $? -eq 0 ]; then
+    ./exec.sh $conffile 8
+fi
