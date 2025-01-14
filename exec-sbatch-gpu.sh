b752a9add886e0f5d77652519f4485ecb0108490
Olivier Filangi <olivier.filagi@inrae.fr>
2025-01-14 14:46:44 +0100

    add test younes

--- exec-sbatch-gpu.sh
+++ exec-sbatch-gpu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #SBATCH --job-name=similarities_compute
-#SBATCH --chdir=/scratch/ofilangi/workspace/encoder-ontology-match-abstract
+#SBATCH --chdir=/scratch/ofilangi/encoder-ontology-match-abstract
 #SBATCH --output=planteome_annot_pubmeb.txt
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
 #SBATCH --mem=10G
 
-## sbatch -p gpu exec.sh
+## sbatch -p gpu exec-sbatch-gpu.sh
 
 . /local/env/envpython-3.9.5.sh
-conffile=./pubmed-all.json
+conffile=./config/pubmed-all.json
 ./exec.sh $conffile 1
 
 if [ $? -eq 0 ]; then
