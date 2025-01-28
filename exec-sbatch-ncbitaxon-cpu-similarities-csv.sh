156221a621db8ef8f6e515397ec43497a07a96d6
Olivier Filangi <olivier.filagi@inrae.fr>
2025-01-28 15:09:27 +0100

    update params exec-sbatch genouest

--- exec-sbatch-ncbitaxon-cpu-similarities-csv.sh
+++ exec-sbatch-ncbitaxon-cpu-similarities-csv.sh
@@ -4,8 +4,8 @@
 #SBATCH --chdir=/scratch/ofilangi/encoder-ontology-match-abstract
 #SBATCH --output=ncbitaxon_annot_pubmed_CPU_4_6.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=50G 
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=60G 
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=olivier.filangi@inrae.fr
 
@@ -13,6 +13,9 @@
 
 . /local/env/envpython-3.9.5.sh
 conffile=./config/ncbi-taxon-pubmed-all.json
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
 #CPU
 ## similarities
 ./exec.sh $conffile 4
--- exec-sbatch-planteome-cpu-similarities-csv.sh
+++ exec-sbatch-planteome-cpu-similarities-csv.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name=PLANTEOME_SIM_CPU
 #SBATCH --chdir=/scratch/ofilangi/encoder-ontology-match-abstract
-#SBATCH --output=PLANTEOME_annot_pubmed_CPU_4_6.txt
+#SBATCH --output=planteome_annot_pubmed_CPU_4_6.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50G 
+#SBATCH --mem=120G 
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=olivier.filangi@inrae.fr
 
@@ -13,6 +13,9 @@
 
 . /local/env/envpython-3.9.5.sh
 conffile=config/planteome-pubmed-all.json
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
 #CPU
 ## similarities
 ./exec.sh $conffile 4
